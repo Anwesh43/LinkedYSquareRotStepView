d36ddf80d43fb523337a453d662b27cf902f8876
Anwesh43 <anweshthecool0@gmail.com>
2018-12-07 23:44:41 +0530

    adding drawYSRSNode canvas extension method

--- ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
+++ ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
@@ -29,3 +29,28 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
+
+fun Canvas.drawYSRSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = Math.min(w, h) / nodes
+    val size : Float = gap / sizeFactor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = color
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val xGap : Float = (w - paint.strokeWidth) / (lines - 1)
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    for (j in 0..lines) {
+        val sc : Float = sc1.divideScale(j, lines)
+        val sf : Float = 1f - 2 * (j % 2)
+        save()
+        translate(xGap * j + paint.strokeWidth/2, 0f)
+        drawLine(0f, 0f, 0f, size * sf * sc, paint)
+        restore()
+    }
+    restore()
+}
