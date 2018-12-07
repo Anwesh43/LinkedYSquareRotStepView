190cb65a0c2696addd5e268ac95e6c981b58cd5c
Anwesh43 <anweshthecool0@gmail.com>
2018-12-08 00:27:57 +0530

    adding YSquareRotStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedysquarerotstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedysquarerotstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedysquarerotstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ysquarerotstepview.YSquareRotStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        YSquareRotStepView.create(this)
     }
 }
