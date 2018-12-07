18648f5dae4eb2a5e8161ecd19ff278016c99a00
Anwesh43 <anweshthecool0@gmail.com>
2018-12-07 11:46:04 +0530

    adding Float and Int extension methods

--- ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
+++ ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
@@ -19,3 +19,13 @@ val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.4f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
