5e3337773587e19635d17eff605eab7d0ceff7eb
Anwesh43 <anweshthecool0@gmail.com>
2018-12-07 10:51:43 +0530

    adding libraries and defining constants

--- ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
+++ ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.ysquarerotstepview
 /**
  * Created by anweshmishra on 07/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 3
+val color : Int = Color.parseColor("#0D47A1")
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.4f
