a9eaaac276d55b995c1e1d85f48f3be6485fd5fe
Anwesh43 <anweshthecool0@gmail.com>
2018-12-07 23:47:31 +0530

    adding YSquareRotStepView class

--- ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
+++ ysquarerotstepview/src/main/java/com/anwesh/uiprojects/ysquarerotstepview/YSquareRotStepView.kt
@@ -54,3 +54,21 @@ fun Canvas.drawYSRSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class YSquareRotStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
